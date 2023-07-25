79816d3b51095688fd16a83ee3581d0c581bf1a3
Zhiying <melody9306@gmail.com>
2023-07-25 23:47:43 +0800

    update page

--- car.php
+++ car.php
@@ -2,6 +2,24 @@
 	session_start();
 	$Id=$_GET['Id'];
 
+	if(!isset($_SESSION['Name']) ){
+		$_SESSION["IDA"] = array();
+		$_SESSION["NameA"]= array();
+		$_SESSION["PriceA"]= array();
+	}
+	
+	$ID=$_SESSION["IDA"] ;
+	$ID[]=$_SESSION["ID"];
+	$_SESSION["IDA"]=$ID;
+
+	$Name=$_SESSION["NameA"] ;
+	$Name[]=$_SESSION["Name"];
+	$_SESSION["NameA"]=$Name;
+
+	$Price=$_SESSION["PriceA"] ;
+	$Price[]=$_SESSION["Price"];
+	$_SESSION["PriceA"]=$Price;
+
 	$_SESSION['Id']=$Id;
 	switch (substr($Id,0,2)){
 		case 'bo':
